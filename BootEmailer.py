8f74ac1742c5ce46b6a13e26d6b8346450cf9a1c
Gary Davis <gary@webguild.com>
2019-05-19 12:56:18 -0400

    V1.1 - Add HostName in message Ignore 169.254.x.x Local Link IPs and instead wait for DHCP-assigned IP

--- BootEmailer.py
+++ BootEmailer.py
@@ -1,10 +1,13 @@
+# BootEmailer.py
+#   Version: V1.1 - 2019-05-19
+#   Author: Gary Davis (gary@webguild.com)
+
 # Python3 script to send email when Pi boots with Pi's IP Address
 # and the SSID of the connected WiFi.
 #
 # Add this to end of root's crontab to run this at boot time:
 #   > sudo crontab -e
-#   @reboot sudo /usr/bin/python3 /home/pi/bin/BootEmailer.py > /home/pi/logs/BootEmailer.log 2>&1
-
+#   @reboot sudo /usr/bin/python3 ~pi/bin/BootEmailer.py > ~pi/logs/BootEmailer.log 2>&1
 
 import smtplib
 import subprocess
@@ -21,18 +24,23 @@ mailTo = "youremail@gmail.com, youremail@somewhere.com" # May have multiple deli
 
 for i in range(30): # Keep trying till it works (up to 1 min)
 	response = subprocess.run("hostname -I", stdout=subprocess.PIPE,
-		shell=True, universal_newlines = True)
-	if (response.stdout.strip() != ""):
+		shell=True, universal_newlines=True)
+	ipAddr = response.stdout.strip()
+	if (not (ipAddr == "" or ipAddr.startswith("169.256."))): # Break when legit DHCP-assigned IP addr
 		break;
-	print("Wait 2 seconds...")
+	print("Wait 2 seconds..." + ipAddr)
 	sleep(2) # Wait for hostname and DNS for SMTP call
 
-my_ip = 'RaspberryPi IP is ' +  response.stdout.strip()
+response = subprocess.run("hostname", stdout=subprocess.PIPE,
+	shell=True, universal_newlines=True)
+hostName = response.stdout.strip()
 
 response = subprocess.run("iwgetid -r", stdout=subprocess.PIPE,
-	shell=True, universal_newlines = True)
+	shell=True, universal_newlines=True)
+ssid = response.stdout.strip()
 
-my_ip += ' and connected to SSID "' + response.stdout.strip() + '"'
+my_ip = hostName + ' IP is ' +  ipAddr + ' and connected to SSID "' + ssid + '"'
+#my_ip = f'{hostName} IP is {ipAddr} and connected to WiFi SSID "{ssid}"' # Python 3.6+
 msg = MIMEText(my_ip)
 msg['From'] = mailFrom
 msg['To'] = mailTo
